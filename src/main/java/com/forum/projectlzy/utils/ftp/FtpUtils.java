47bfc3fa3fd794833fdc18fdd59456314fa7d8b6
lzy <848539845@qq.com>
2022-07-26 19:44:04 +0800

    feat(util): 新增ftp工具类

---
+++ src/main/java/com/forum/projectlzy/utils/ftp/FtpUtils.java
@@ -0,0 +1,86 @@
+package com.forum.projectlzy.utils.ftp;
+
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+/**
+ * ftp工具类
+ */
+@Component
+public class FtpUtils {
+
+    /**
+     * 基础路径
+     */
+    @Value("${ftp.basePath}")
+    private String ftpBasePath;
+
+    @Autowired
+    private FtpPool ftpPool;
+
+    /**
+     * 文件类型追加路径
+     */
+    public static String IMG_PATH = "/img";
+
+    /**
+     * 视频类型追加路径
+     */
+    public static String VIDEO_PATH = "/video";
+
+
+    /**
+     * 上传
+     * @param map  String 为文件名 , InputStream 为文件输入流
+     * @param path 追加路径
+     */
+    public void upload(Map<String, InputStream> map, String path) {
+
+        try {
+
+            FTPClient ftp = ftpPool.getFtpClient();
+            //创建文件夹
+            ftp.makeDirectory(ftpBasePath + path);
+            //存储文件
+            ftp.changeWorkingDirectory(ftpBasePath + path);
+            map.forEach((fileName, inputStream) -> {
+                try {
+                    ftp.storeFile(fileName, inputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            ftpPool.returnFtpClient(ftp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 读取/下载
+     * @param fileName 文件名
+     * @param path 追加路径
+     * @return
+     */
+    public InputStream download(String fileName, String path) {
+        InputStream inputStream = null;
+        try {
+            FTPClient ftpClient = ftpPool.getFtpClient();
+            ftpClient.changeWorkingDirectory(ftpBasePath + path);
+            inputStream = ftpClient.retrieveFileStream(fileName);
+            ftpPool.returnFtpClient(ftpClient);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inputStream;
+    }
+
+}
