4884c3a577a5b0497e292083f891c790f3a2b434
lzy <848539845@qq.com>
2022-08-03 21:32:14 +0800

    feat(posting):新增posting的dto类(存储简单的帖子信息传送至客户端)

---
+++ src/main/java/com/forum/projectlzy/entity/dto/SimplePostingDto.java
@@ -0,0 +1,24 @@
+package com.forum.projectlzy.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimplePostingDto {
+private Integer id;
+
+    private String title;
+
+    private String simpleContent;
+
+    private String fileHtmlString;
+
+    private Integer like;
+
+    private Date lastPublishTime;
+}
